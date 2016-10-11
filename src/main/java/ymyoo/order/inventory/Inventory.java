4a08fd1d1893c3471d6b1722e2ac83cab17c519e
Young-mo Yoo <gigamadness@gmail.com>
2016-10-11 09:27:41 +0900

    Console 로그 가독성 높임

--- src/main/java/ymyoo/order/inventory/Inventory.java
+++ src/main/java/ymyoo/order/inventory/Inventory.java
@@ -15,8 +15,8 @@ public class Inventory {
      */
     public void check(OrderItem product) {
         try { Thread.sleep(250); } catch (InterruptedException e) {}
-        System.out.println("[Current Thread ID - " + Thread.currentThread().getId() + "][Inventory Task]" +
-                "상품 번호" + product.getProductId() + "-> 상품 재고 확인");
+        System.out.println("[Current Thread ID - " + Thread.currentThread().getId() + "][Inventory Task]-재고 확인-" +
+                "상품 번호" + product.getProductId());
     }
 
     /**
@@ -25,7 +25,7 @@ public class Inventory {
      */
     public void reserve(OrderItem product) {
         try { Thread.sleep(250); } catch (InterruptedException e) {}
-        System.out.println("[Current Thread ID - " + Thread.currentThread().getId() + "][Inventory Task]" +
-                "상품 번호" + product.getProductId() + "-> 재고 확보 " + product.getOrderCount() + "개");
+        System.out.println("[Current Thread ID - " + Thread.currentThread().getId() + "][Inventory Task]-재고 확보-" +
+                "상품 번호" + product.getProductId() + " " + product.getOrderCount() + "개");
     }
 }
