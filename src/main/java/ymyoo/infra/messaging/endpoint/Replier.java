570ab6556bae0e33d2165f5921894cefd7678890
Young-mo Yoo <gigamadness@gmail.com>
2016-12-20 13:39:28 +0900

    [리펙토링] Requester/Replier 추가

---
+++ src/main/java/ymyoo/infra/messaging/endpoint/Replier.java
@@ -0,0 +1,7 @@
+package ymyoo.infra.messaging.endpoint;
+
+/**
+ * Created by 유영모 on 2016-12-20.
+ */
+public class Replier {
+}
---
+++ src/main/java/ymyoo/order/messaging/endpoint/Requestor.java
@@ -0,0 +1,7 @@
+package ymyoo.order.messaging.endpoint;
+
+/**
+ * Created by 유영모 on 2016-12-20.
+ */
+public class Requestor {
+}
