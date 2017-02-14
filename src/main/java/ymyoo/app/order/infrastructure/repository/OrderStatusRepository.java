3f940cfd784e7e8e0d172c83734ccbed71058c3c
Young-mo Yoo <gigamadness@gmail.com>
2017-02-14 11:14:54 +0900

    https://github.com/DomainDriven/AsyncOrder/issues/5 추가 적용

--- src/main/java/ymyoo/app/order/infrastructure/repository/OrderStatusRepository.java
+++ src/main/java/ymyoo/app/order/infrastructure/repository/OrderStatusRepository.java
@@ -1,19 +1,17 @@
 package ymyoo.app.order.infrastructure.repository;
 
 import ymyoo.app.order.domain.OrderStatus;
-import ymyoo.persistence.GlobalEntityManagerFactory;
-
-import javax.persistence.EntityManager;
+import ymyoo.persistence.TransactionJpaTemplate;
 
 /**
  * Created by 유영모 on 2017-01-11.
  */
 public class OrderStatusRepository {
     public OrderStatus find(final String orderId) {
-        EntityManager em = GlobalEntityManagerFactory.getEntityManagerFactory().createEntityManager();
-        OrderStatus orderStatus = em.find(OrderStatus.class, orderId);
-        em.close();
+        TransactionJpaTemplate jpaTemplate = new TransactionJpaTemplate();
 
-        return orderStatus;
+        return jpaTemplate.execute((em)-> {
+            return em.find(OrderStatus.class, orderId);
+        });
     }
 }
