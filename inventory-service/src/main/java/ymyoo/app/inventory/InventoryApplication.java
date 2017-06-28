681784b874575cba57440e1fa073de984b648fb3
yooyoung-mo <gigamadness@gmail.com>
2017-06-29 08:47:45 +0900

    Inventory Service 를 Spring Boot 적용

--- inventory-service/src/main/java/ymyoo/app/inventory/InventoryApplication.java
+++ inventory-service/src/main/java/ymyoo/app/inventory/InventoryApplication.java
@@ -1,13 +1,31 @@
 package ymyoo.app.inventory;
 
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 import ymyoo.app.inventory.adapter.messaging.InventoryReplier;
 
 /**
  * Created by yooyoung-mo on 2017. 6. 23..
  */
+@SpringBootApplication
 public class InventoryApplication {
-  public static void main(String[] args) {
-    Thread inventoryReplier = new Thread(new InventoryReplier());
-    inventoryReplier.start();
-  }
+//    public static void main(String[] args) {
+//        Thread inventoryReplier = new Thread(new InventoryReplier());
+//        inventoryReplier.start();
+//    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(InventoryApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+        return args -> {
+            Thread inventoryReplier = new Thread(new InventoryReplier());
+            inventoryReplier.start();
+        };
+    }
 }
